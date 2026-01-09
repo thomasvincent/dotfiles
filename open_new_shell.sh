8093daac011d7887584cfc10e5568c42d9a4eff6
Thomas Vincent <thomasvincent@gmail.com>
2026-01-08 18:46:02 -0800

    chore: remove obsolete one-time scripts
    - fix-code-style.sh: pre-commit handles this now
    - open_new_shell.sh: simple test helper no longer needed
    - verify_fixes.sh: one-time debugging script

--- open_new_shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Open a new shell to test our dotfiles
-
-echo "Opening a new ZSH interactive shell..."
-echo "Type 'echo \$PLATFORM' to test if platform detection works"
-echo "Type 'exit' to return to the previous shell"
-echo ""
-
-/bin/zsh -i
